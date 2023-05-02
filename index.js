12c05bcea01fa4c65fffda71ae4b521a3d2ca3af
SadoninS <stanislavsadonin@gmail.com>
2023-05-02 13:41:07 +0400

    1

---
+++ index.js
@@ -0,0 +1,24 @@
+let increaseBtn = document.querySelector('.increase-value')
+let resetBtn = document.querySelector('.reset-value')
+let closeAppBtn = document.querySelector('.close-app')
+let countertext = document.querySelector('.value')
+counter = 0
+
+
+increaseBtn.addEventListener('click', function() {
+    counter = counter + 1
+    countertext.innerHTML = counter
+})
+
+resetBtn.addEventListener('click', function() {
+countertext.innerHTML = 0
+})
+
+closeAppBtn.addEventListener('click', function() {
+    window. close()
+})
+
+
+// window. close()
+
+
